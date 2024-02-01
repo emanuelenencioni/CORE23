75670f6fda9ee836c72ebea34d6d563949230991
IngLorenzoPorcheddu <lorenzo.porcheddu.00@gmail.com>
2024-02-01 23:56:08 +0100

    Changed to DMA

--- Core/Src/ADCTask.c
+++ Core/Src/ADCTask.c
@@ -2,36 +2,30 @@
 #include "task.h"
 #include "stm32f7xx_hal.h"
 
-#define ADC_BUFFER_SIZE 10
+#define ADC_BUFFER_SIZE 12
 
 // Define the ADC buffer
 uint16_t adcBuffer[ADC_BUFFER_SIZE];
+// Define the ADC buffer
+uint16_t adcReadings[ADC_BUFFER_SIZE];
 
 // ADC task function
 void ADCTask(void *pvParameters)
 {
     while (1)
     {
-        if(xSemaphoreTake(ASCanSemHandle, (TickType_t) WAIT_FOR_PILOT_STATE) == pdTRUE){
+        void vAnalogRead(void *argument)
+        {
+        /* Infinite loop */
 
-            // Read all ADC channels
-            for (uint8_t channel = 0; channel < ADC_BUFFER_SIZE; channel++)
+            xSemaphoreTake(xsemaph_adc, portMAX_DELAY);
+            for(int i=0;i<12;i++)
             {
-                // Start ADC conversion for the current channel
-                HAL_ADC_Start(&hadc1);
-
-                yield();
-                
-                // Wait for ADC conversion to complete
-                HAL_ADC_PollForConversion(&hadc1, 1);
-                
-                // Read the ADC value for the current channel
-                adcBuffer[channel] = HAL_ADC_GetValue(&hadc1);
-
-                HAL_ADC_Stop(&hadc1);
+                adcReadings[i] = adcBuffer[i];
             }
+            xSemaphoreGive(xsemaph_adc);
         }
-        // Pass the ADC values to other tasks
+        /* USER CODE END StartLeAnalog */
         
         // Delay for 50ms
         vTaskDelay(pdMS_TO_TICKS(50));
@@ -43,3 +37,5 @@ void CreateADCTask()
 {
     xTaskCreate(ADCTask, "ADCTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
 }
+
+https://electronics.stackexchange.com/questions/581003/stm32h7-adc-with-dma-reading-only-zeros-using-hal-and-freertos
