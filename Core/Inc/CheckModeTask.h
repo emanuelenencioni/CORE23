51b4c130481b9fd3e6f6a9b74c6682298dab59fc
Emanuele Nencioni <emanencio@hotmail.com>
2024-02-06 22:04:32 +0100

    Added CheckModeTask

--- Core/Inc/CheckModeTask.h
+++ Core/Inc/CheckModeTask.h
@@ -2,9 +2,26 @@
 #define CHECKMODETASK_H_
 
 
+#include "CanHandlerTask.h"
+
+
 #define WAIT_FOR_PILOT_STATE 50 
 
 
+
 void checkModeThread(void* argument);
+
+/**
+ * @brief Read data from ASCanBuffer, remember to execute this in critical section.
+ * 
+ */
+void readModeData();
+
+/**
+ * @brief Function that send the corrispective error code to the Pilot23 board.
+ * 
+ * @param errorCode Look at the CAN-ID table for reference
+ */
+void sendErrorToPilot(uint8_t errorCode);
 
 #endif
--- Core/Src/CheckModeTask.c
+++ Core/Src/CheckModeTask.c
@@ -1,38 +1,97 @@
 #include "CheckModeTask.h"
-#include "cmsis_os.h"
-#include "main.h"
-#include "task.h"
-#include "semphr.h"
-#include "CanHandlerTask.h"
 
-extern osMessageQueueId_t canTxEngineQueue;
+
+//CAN variables
 extern osMutexId_t ASCanSemHandle;
+extern osMutexId_t EngCanSemHandle;
+
 extern ASCANBuffer AutCanBuffer;
+extern EngineCANBuffer EngCANBuffer;
+
+extern osMessageQueueId_t canTxASQueue;
+
+//Can message for sending state error to the pilot23
+CANMessage msg;
+
+uint8_t selectedMission;
+
+enum Mode {NotSelected = 0, Manual = 1, Autonomous = 2};
+
+enum  Mode actualMode;
+enum Mode reqMode; 
 
 
-enum state {Manual = 0, Autonomous = 1, NotSelected = 2};
 
 void checkModeThread(void* argument){
 
     TickType_t xLastWakeTime;
 	const TickType_t xFrequency = 20;
 
-    enum state actualState = NotSelected;
-    enum state reqState = Manual;
+    CAN_TxHeaderTypeDef header;
+    header.StdId = 400;
+    header.DLC = 8;
+    msg.header = header;
+    
+    actualMode = NotSelected;
+    reqMode = NotSelected;
+
+
+    msg.data[0] = 10;
+
+    int AS_PIN = 0;
 
     while(1){
         // Reading the state from  PILOT23
-        if(xSemaphoreTake(ASCanSemHandle, (TickType_t) WAIT_FOR_PILOT_STATE) == pdTRUE){
-            //ReadingModeData
-            actualState = AutCanBuffer.reqState % 3;
-            xSemaphoreGive(ASCanSemHandle);
-        }
+        if(actualMode == NotSelected){
+            if(xSemaphoreTake(ASCanSemHandle, (TickType_t) WAIT_FOR_PILOT_STATE) == pdTRUE){
+                //Critical section
+                readModeData();
+                xSemaphoreGive(ASCanSemHandle);
+            }
+            if(reqMode != NotSelected) {
+                actualMode = reqMode;
+            }
+            else{
+                if(xSemaphoreTake(EngCanSemHandle, (TickType_t) 0) == pdTRUE){
+                    //ReadingModeData
+                    if(EngCANBuffer.RPM > MIN_RPM_ENG_ON){ // TS active while no Mode selected
+                        sendErrorToPilot(12);
+                    }
+                }
 
-        if(reqState == Autonomous) {
-            
+                // if( __NOP()){ // TODO ASMS on and no mode selected
+                //     sendErrorToPilot(10);
+                // }
+            }
         }
+        else{ // Misison already selected, to change mission is necessary reset the board.
+            // if(actualMode == Manual){
+            //     if(__NOP()){  // TODO ASMS on in manual mode
+            //         sendErrorToPilot(10);
+            //     }
+            // }
 
+            if(actualMode == Autonomous){
+                if(xSemaphoreTake(EngCanSemHandle, (TickType_t) 0) == pdTRUE){
+                    //ReadingModeData
+                    if(EngCANBuffer.RPM > MIN_RPM_ENG_ON){ // TODO TS active while no ASMS OFF
+                        sendErrorToPilot(11);
+                    }
+                }
+            }
+        }
     }
-
     vTaskDelayUntil(xLastWakeTime, xFrequency);
+}
+
+void readModeData() {
+    reqMode = AutCanBuffer.reqMode % 3;
+    selectedMission = AutCanBuffer.selectedMission;
+}
+
+void sendErrorToPilot(uint8_t errorCode) {
+    msg.data[0] = errorCode;
+    if(osMessageQueuePut(canTxASQueue, &msg, NULL, 0) != HAL_OK){
+        // TODO comm. error
+    }
 }
