9c9f01bfc770ea603f556e73faac36cbff638e02
Emanuele Nencioni <emanencio@hotmail.com>
2024-02-06 22:03:31 +0100

    removed Interrupt handlers

--- Core/Inc/stm32f7xx_it.h
+++ Core/Inc/stm32f7xx_it.h
@@ -52,8 +52,6 @@ void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
-void EXTI0_IRQHandler(void);
-void EXTI1_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
 void DMA2_Stream0_IRQHandler(void);
 /* USER CODE BEGIN EFP */
