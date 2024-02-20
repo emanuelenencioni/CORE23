6976a73ccf99e58847079235a2cf173ee7fa1dcd
IngLorenzoPorcheddu <lorenzo.porcheddu.00@gmail.com>
2024-02-20 18:03:29 +0100

    AccTask V1

--- Core/Inc/AccTask.h
+++ Core/Inc/AccTask.h
@@ -4,4 +4,16 @@
 #include "task.h"
 #include "semphr.h"
 #include "queue.h"
+#include "ADCTask.h"
+#include "CanHandlerTask.h"
+#include "stdlib.h"
 
+#define APPS1 5
+#define APPS2 6
+#define MAX_DEVIATION 400 // 10% deviation
+
+#ifndef ACCTASK_H_
+#define ACCTASK_H_
+
+void AccThread(void* argument);
+#endif
--- Core/Src/AccTask.c
+++ Core/Src/AccTask.c
@@ -1,2 +1,70 @@
 #include "AccTask.h"
 
+//adc buffer from dma
+extern uint16_t adcReadings[ADC_BUFFER_SIZE];
+
+//CAN variables
+extern osMutexId_t ASCanSemHandle;
+extern osMutexId_t EngCanSemHandle;
+
+extern ASCANBuffer AutCanBuffer;
+extern EngineCANBuffer EngCANBuffer;
+
+extern QueueHandle_t canTxASQueue;
+
+extern osMutexId_t ADCSemHandle;
+
+//Can message for sending state error to the pilot23
+CAN_TxHeaderTypeDef header;
+CANMessage msgAcc;
+
+// Acc task function
+void AccThread(void* argument) {
+    
+    TickType_t xLastWakeTime;
+	const TickType_t xFrequency = 100;
+    uint32_t timer = 0;
+    uint16_t apps1 = 0;
+    uint16_t apps2 = 0;
+
+    // Settings for can message
+    header.StdId = 400;
+    header.ExtId = 0;
+    header.IDE = 0;
+    header.RTR = 0;
+    header.DLC = 1;
+    msgAcc.header = header;
+    
+
+    xLastWakeTime = xTaskGetTickCount();
+    while (1) {
+        vTaskDelayUntil( &xLastWakeTime, xFrequency);
+        /* Infinite loop */
+
+            if(xSemaphoreTake(ADCSemHandle, (TickType_t) 0) == pdTRUE) {
+                
+                apps1 = adcReadings[APPS1];
+                apps2 = adcReadings[APPS2];
+                DAC->DHR12R2 = (apps1+apps2)/2; // Set DAC channel 2 value to average of APPS1 and APPS2
+                //DAC->DHR12R1: channel 1 12-bit right-aligned data holding register
+                //DHR12R2: channel 2 12-bit right-aligned data holding register
+                
+                
+                // check plausibility APPS
+                if (abs(apps1 - apps2) > MAX_DEVIATION) {
+                    timer++;
+                    if (timer > 10) { // 100ms implausibility
+                        HAL_GPIO_WritePin(SHUTDOWN_CMD_GPIO_Port, SHUTDOWN_CMD_Pin, GPIO_PIN_RESET); // Shutdown OFF
+                        msgAcc.data[0] = 13; // Error code
+                        xQueueSend(canTxASQueue, &msgAcc, 0); // Send error message
+                    }
+                }
+                else {
+                    timer = 0;
+                }
+
+                //do something with adcReadings
+                xSemaphoreGive(ADCSemHandle);
+        }
+    }
+}
