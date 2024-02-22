f9fbad2c6c7c86cf1b870465a1f8de07d789c896
Emanuele Nencioni <emanencio@hotmail.com>
2024-02-22 11:36:06 +0100

    refactory ADCTask

--- Core/Src/ADCTask.c
+++ Core/Src/ADCTask.c
@@ -1,21 +1,29 @@
 #include "ADCTask.h"
 
+
+/**
+ * ADCBuffer sensors:
+ * - 4: EBS Air 2
+ * - 5: EBS Air 1
+ * - 
+ */
+
 extern ADC_HandleTypeDef hadc1;
 extern osMutexId_t ADCSemHandle;
 // Define the ADC buffer for DMA
-volatile uint16_t adcBuffer[ADC_BUFFER_SIZE];
+
 
 // Define the ADC buffer in critical section
-uint16_t adcReadings[ADC_BUFFER_SIZE];
+ADCBuffer adcReadings;
 
 // ADC task function
 void ADCThread(void* argument) {
     
     TickType_t xLastWakeTime;
 	const TickType_t xFrequency = 100;
-
+    volatile uint16_t adcBuffer[ADC_BUFFER_SIZE];
     HAL_ADC_Start_DMA(&hadc1, (uint16_t*)adcBuffer, sizeof(adcBuffer)/sizeof(adcBuffer[0]));
-
+    
 
     xLastWakeTime = xTaskGetTickCount();
     while (1) {
@@ -23,12 +31,21 @@ void ADCThread(void* argument) {
         /* Infinite loop */
 
             if(xSemaphoreTake(ADCSemHandle, (TickType_t) 0) == pdTRUE) {
-                for(int i=0;i<12;i++) {
-                    adcReadings[i] = adcBuffer[i];
-                }
+                // for(int i=0;i<12;i++) {
+                //     adcReadings[i] = adcBuffer[i];
+                // }
+                //TODO sapere le map per lo stronzo
+
+                adcReadings.EBSAir2 = map(adcBuffer[4], 0, 4096, 0, 100); // TODO vedere fondo scala sensori EBS
+                adcReadings.EBSAir1 = map(adcBuffer[5], 0, 4096, 0, 100); //EBS_AIR2
+
                 xSemaphoreGive(ADCSemHandle);
         }
     }
 }
 
 // https://electronics.stackexchange.com/questions/581003/stm32h7-adc-with-dma-reading-only-zeros-using-hal-and-freertos
+
+float map(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
