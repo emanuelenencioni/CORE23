33b66833221b7b17873f965b4b85e827dbc1aeb3
IngLorenzoPorcheddu <lorenzo.porcheddu.00@gmail.com>
2024-02-22 12:22:14 +0100

    Updated Gear Task

--- Core/Inc/GearTask.h
+++ Core/Inc/GearTask.h
@@ -5,17 +5,20 @@
 #include "semphr.h"
 #include "queue.h"
 #include "CanHandlerTask.h"
+#include "ADCTask.h"
 //#include "stdlib.h"
 
 #ifndef GEARTASK_H_
 #define GEARTASK_H_
 
-#define cutoff_duration 	100		//ms to hold up cutoff signal
-#define cutoff_wait			200		//ms before actuate upshift
-#define shift_hold			500		//ms to hold the gear pressed
+#define CUTOFF_TIME 	100		//ms to hold up cutoff signal
+//#define cutoff_wait			200		//ms before actuate upshift
+#define SHIFT_HOLD			500		//ms to hold the gear pressed
 #define clutch_engaging 	3000	//DAC value when clutch starts to grip
 #define clutch_releasing 	300		//DAC value when clutch has about 100% grip (but not fully released)
 
 void gearThread(void* argument);
 void upShift();
+void downShift();
+uint8_t gear_getCurrent();
 #endif
--- Core/Src/GearTask.c
+++ Core/Src/GearTask.c
@@ -9,18 +9,32 @@ extern EngineCANBuffer EngCANBuffer;
 
 extern QueueHandle_t canTxASQueue;
 
+extern osMutexId_t ADCSemHandle;
 //Can message for sending state error to the pilot23
-CAN_TxHeaderTypeDef header;
+
+//adc buffer from dma
+extern ADCBuffer adcReadings;
+
+uint16_t desmo1;
+uint16_t desmo2;
+
 CANMessage msgGear;
 
+uint16_t gear_pos[] = {3260, 3620, 200, 875, 1570, 2250}; // Gear position values
+
 void gearThread(void* argument) {
-    
+    CAN_TxHeaderTypeDef header;
+
     TickType_t xLastWakeTime;
 	const TickType_t xFrequency = 10;
 
     // Gear task function
     uint8_t request_upshift = 0;
     uint8_t request_downshift = 0;
+    uint8_t force_mode = 0;
+
+    desmo1 = 0;
+    desmo2 = 0;
 
     xLastWakeTime = xTaskGetTickCount(); // rate of execution
 
@@ -28,23 +42,65 @@ void gearThread(void* argument) {
 
         if(xSemaphoreTake(ASCanSemHandle, (TickType_t) 0) == pdTRUE) {
 
-        request_downshift = AutCanBuffer.reqDownShift;
-        request_upshift = AutCanBuffer.reqUpShift;
+            request_downshift = AutCanBuffer.reqDownShift;
+            request_upshift = AutCanBuffer.reqUpShift;
+            force_mode = AutCanBuffer.reqDownShift;
 
-        xSemaphoreGive(ASCanSemHandle);
+            xSemaphoreGive(ASCanSemHandle);
+        }
 
-        } 
+        if(xSemaphoreTake(ADCSemHandle, (TickType_t) 0) == pdTRUE) {
+
+            desmo1 = adcReadings.desmo1;
+            desmo2 = adcReadings.desmo2;
+
+            xSemaphoreGive(ADCSemHandle);
+        }
+
+        xQueueSend(canTxASQueue, &msgGear, 0); // TODO: check the queue
 
     }
 
+    if(request_upshift == 1){
+        upShift();
+    }
+    else if(request_downshift == 1){
+        downShift();
+    }
+    
 }
 
 void upShift() {
-    // Set the gear to the next gear
+    // Gear upshift with cut-off
     HAL_GPIO_WritePin(CUTOFF_GPIO_Port, CUTOFF_Pin, GPIO_PIN_SET);
-    vTaskDelay(100); // Cut-off Duration
+    vTaskDelay(CUTOFF_TIME); // Cut-off Duration
     HAL_GPIO_WritePin(VUVG_UP_GPIO_Port, VUVG_UP_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(CUTOFF_GPIO_Port, CUTOFF_Pin, GPIO_PIN_RESET);
-    vTaskDelay(100); // Upshift Duration
+    vTaskDelay(SHIFT_HOLD); // Upshift Duration
     HAL_GPIO_WritePin(VUVG_UP_GPIO_Port, VUVG_UP_Pin, GPIO_PIN_RESET);
+}
+
+void downShift() {
+    // Gear downshift with cut-off
+    HAL_GPIO_WritePin(CUTOFF_GPIO_Port, CUTOFF_Pin, GPIO_PIN_SET);
+    vTaskDelay(CUTOFF_TIME); // Cut-off Duration
+    HAL_GPIO_WritePin(VUVG_DOWN_GPIO_Port, VUVG_UP_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(CUTOFF_GPIO_Port, CUTOFF_Pin, GPIO_PIN_RESET);
+    vTaskDelay(SHIFT_HOLD); // Upshift Duration
+    HAL_GPIO_WritePin(VUVG_DOWN_GPIO_Port, VUVG_UP_Pin, GPIO_PIN_RESET);
+}
+
+uint8_t gear_getCurrent(){
+	//find minimum error to guess the current gear
+	uint16_t error = 4095;
+	uint8_t  guessed_gear;
+
+	for (uint8_t tmp_gear = 0; tmp_gear <= 5; tmp_gear++){
+		if (abs(gear_pos[tmp_gear]- desmo1) <= error){
+			error = abs(gear_pos[tmp_gear]- desmo1);
+			guessed_gear = tmp_gear;
+		}
+
+	}
+	return guessed_gear;
 }
