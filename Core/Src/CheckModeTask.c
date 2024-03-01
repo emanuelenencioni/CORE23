2c93b84029486ae5c38096d9c215cc6447938916
Emanuele Nencioni <emanencio@hotmail.com>
2024-03-01 17:46:30 +0100

    finished obj diag

--- Core/Src/CheckModeTask.c
+++ Core/Src/CheckModeTask.c
@@ -29,7 +29,9 @@ uint8_t selectedMission;
 enum  Mode actualMode;
 enum Mode reqMode; 
 
-
+//FOR TESTING
+extern CAN_HandleTypeDef hcan2;
+extern TIM_HandleTypeDef htim5;
 
 void checkModeThread(void* argument){
 
@@ -57,6 +59,14 @@ void checkModeThread(void* argument){
     // Default error message
     msg.data[0] = 10;
 
+    //FOR TESTING
+    HAL_TIM_Base_Start(&htim5);
+    uint32_t start_time = 0;
+    uint32_t finish_time = 0;
+    uint16_t count = 0;
+    uint32_t avg = 0;
+    uint8_t avg_count = 100;
+
     xLastWakeTime = xTaskGetTickCount();
     while(1){
         vTaskDelayUntil( &xLastWakeTime, xFrequency );
@@ -64,11 +74,14 @@ void checkModeThread(void* argument){
         switch(actualMode){
             default:
                 if(xSemaphoreTake(ASCanSemHandle, (TickType_t) WAIT_FOR_PILOT_STATE) == pdTRUE){
+                    start_time = TIM5->CNT;
                     readModeData();
+                    finish_time = TIM5->CNT;
                     xSemaphoreGive(ASCanSemHandle);
                 }
                 if( reqMode == Autonomous || reqMode == Manual) 
                     // UpdateMode
+                    
                     actualMode = reqMode;
                 else{ 
                     if(xSemaphoreTake(EngCanSemHandle, (TickType_t) 0) == pdTRUE){
@@ -122,15 +135,41 @@ void checkModeThread(void* argument){
                     sendErrorToPilot(10);
                 
                 break;
+
+        
+        }
+
+        if(count < avg_count){
+            avg += finish_time - start_time;
+            count++;
+        }
+        else{
+            uint32_t TxMailbox = 0;
+            avg /= avg_count;
+            CANMessage msg;
+            CAN_TxHeaderTypeDef header_test;
+            // Settings for can message
+            header_test.StdId = 400;
+            header_test.ExtId = 0;
+            header_test.IDE = 0;
+            header_test.RTR = 0;
+            header_test.DLC = 4;
+            msg.header = header_test;
+            msg.data[0] = (uint8_t) (avg >> 24);
+            msg.data[1] = (uint8_t) (avg >> 16);
+            msg.data[2] = (uint8_t) (avg >> 8);
+            msg.data[3] = (uint8_t) avg;
+            HAL_CAN_AddTxMessage(&hcan2, &msg.header, msg.data, &TxMailbox);
+            vTaskSuspend(NULL);
         }
     }
 }
 
 void sendErrorToPilot(uint8_t errorCode) {
     msg.data[0] = errorCode;
-    if(xQueueSend(canTxASQueue, &msg, 0) != pdTRUE){
+    //if(xQueueSend(canTxASQueue, &msg, 0) != pdTRUE){
         // TODO comm. error
-    }
+    //}
 }
 
 
@@ -139,8 +178,4 @@ void readModeData() {
         reqMode = 0;
     else
         reqMode = AutCanBuffer.reqMode;
-    if(AutCanBuffer.selectedMission < 0 || AutCanBuffer.selectedMission > 6)
-        selectedMission = 0;
-    else
-        selectedMission = AutCanBuffer.selectedMission;
 }
